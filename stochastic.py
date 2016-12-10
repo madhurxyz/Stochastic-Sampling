4cf1160c7060322901c60b01311961453cdf43e5
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-09 23:28:11 -0800

    added function that returns total number of words in the original words list

--- stochastic.py
+++ stochastic.py
@@ -4,16 +4,26 @@ import sys
 from histogram import get_words_list
 from histogram import create_histogram
 
+def get_count(histogram):
+    count = 0
+    for key,value in histogram.iteritems():
+        count += value
+    return count
+
 def get_random_word(histogram):
     word_list = []
     for key, value in histogram.iteritems():
         word_list.append(key)
-    return words_list
+    rand_index = random.randint(0, len(word_list)-1)
+    random_word = ''.join(word_list[rand_index])
+    return random_word
 
 def my_app(file_name):
     words_list = get_words_list(file_name)
     histogram = create_histogram(words_list)
-    random_word = get_random_word(histogram)
+    # random_word = get_random_word(histogram)
+    count = get_count(histogram)
+    print count
 
 
 
